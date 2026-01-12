80a5ec9f1601cb20a9d3f2d061f74b890995056a
tdpkimhuy <tdpkimhuy@gmail.com>
2026-01-12 18:09:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,12 @@
-const CACHE_NAME = 'kimhuy-shell-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Luôn lấy dữ liệu mới từ mạng, không cache nội dung iframe
+  e.respondWith(fetch(e.request));
 });
